1615b4756a7eae6afa9b4ccd09985dccc4583838
Abiya L.S <165126892+AbiyaLS@users.noreply.github.com>
2025-04-01 21:44:33 +0530

    Create Function.dart

---
+++ Function.dart
@@ -0,0 +1,30 @@
+void main() {
+  /* declare function
+   * datatype function_name(){
+   
+   }
+   calling a function inside the void main(){
+   function_name();
+   }
+  
+//   String name ="Arya";
+//   printName(name,12);
+//    print(name);
+// }
+//  void printName(String name,int age ){
+//  String name1 =  "ASHISH";
+//    print(name);
+ }*/
+  
+//   USING POSITIONAL ARGUMENT
+  String name = "Arya";
+  printName(12,name,"Hello");
+  print(name);
+}
+
+void printName(int age,String name,String greeting){     //in this case the order of the argument is important if the order change then it show error
+  print(age);
+  print(greeting);
+  print(name);
+}
+//Named argument
