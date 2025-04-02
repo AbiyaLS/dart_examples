619c82cd44d271e07c8db1710d6dcc7d55ef1e37
Abiya L.S <165126892+AbiyaLS@users.noreply.github.com>
2025-04-02 08:42:44 +0530

    different type of argument and anonymous function

--- Function.dart
+++ Function.dart
@@ -32,3 +32,49 @@ void printName(int age,String name,String greeting){     //in this case the orde
 // /--------------------------Named argument-----------------------------------
 void printStaff({int? age, required String name,required String greeting}){
   print(age);
+
+  //------------------ USING BOTH POSITION AND NAMED ARGUMENT------------------------
+void main(){
+ studentDetails(23,true,grade:"B",name:"abiya") ;
+
+}
+
+void studentDetails(int age,bool isPassed,{required String name, required String grade}){
+   print(age);
+  print(name);
+}
+
+  // ---------------------Records in Function------------------------
+
+void main(){
+ final staff =stuffDetails();
+  print("${staff.age}\n${staff.name}\n${staff.course}");
+}
+  // it help to store multple values without creating a class 
+
+({int age, String name,  String course} ) stuffDetails(){
+  return (name:"Arya",age: 21,course:"CS");
+}
+
+  // ---------------------Anonymous Function------------------------
+
+void main(){
+final staff =printStaff();
+  print(staff());
+  print((){            
+//     here come the closure error
+    print("Yooo");
+  }());
+  // when we put () then the error change nad give the result
+}
+
+Function printStaff(){
+  return(){ 
+    //conside () as variable for calling that particular () then we want print the () also else there come closure error
+    print("yooo");
+  };
+}
+ // function without name is anonymous function but behave like function 
+//  eg :       (){
+//                print("Hello");
+//               }
